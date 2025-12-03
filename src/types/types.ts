29c98e1ae4afdb1e98715d33eee4acf13d78bb62
KattyAlice <kattyalice@yahoo.com>
2025-12-03 10:36:51 -0500

    Fix types: UserProfile and ProductContext reducer

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,24 +1,18 @@
+// Product Type
 export interface Product {
-    id: string;
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    image: string;
-    rating: {
-        rate: number;
-        count: number;
-    };
+  id: string;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
 }
 
 export type Category = string;
-
-export interface UserProfile {
-  name?: string;
-  address?: string;
-  email: string;
-}
-
 
 export interface UserProfile {
   id: string;
